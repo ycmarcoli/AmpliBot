4d1dbccc8b116b6066e6a38fb53b758eeab99dc8
Marco <realpuredesu@gmail.com>
2024-06-14 09:19:15 -0700

    Change API route name

--- amplibot-react-native/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplibot-react-native/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,6 +1,6 @@
 export type AmplifyDependentResourcesAttributes = {
   "api": {
-    "helloworldapi": {
+    "amplibotapi": {
       "ApiId": "string",
       "ApiName": "string",
       "RootUrl": "string"
