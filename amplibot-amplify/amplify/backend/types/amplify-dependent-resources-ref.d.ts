4c151ee89e8043959480b9b432232c93feb1aa4e
Marco <realpuredesu@gmail.com>
2024-06-14 08:28:29 -0700

    Add HelloWorld API with VPC

--- amplibot-amplify/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplibot-amplify/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "helloworldapi": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    }
+  },
   "auth": {
     "amplibot02abecef": {
       "AppClientID": "string",
@@ -8,6 +15,15 @@ export type AmplifyDependentResourcesAttributes = {
       "UserPoolArn": "string",
       "UserPoolId": "string",
       "UserPoolName": "string"
+    }
+  },
+  "function": {
+    "HelloWorldFunction": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
     }
   }
 }
